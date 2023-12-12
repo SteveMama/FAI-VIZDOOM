3eee03aea071b804be9a2a65721c82d81cffb9ac
SteveMama <pkompally@gmail.com>
2023-12-12 13:01:46 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,19 +27,10 @@ _docs_deps = [
 
 setup(
     # Information
-    name="sample-factory",
-    description="High throughput asynchronous reinforcement learning framework",
+    name="modified Sample Factory for FAI project",
+    description="Reinforcement Learning in Action",
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    version="2.1.1",
-    url="https://github.com/alex-petrenko/sample-factory",
-    author="Aleksei Petrenko",
-    license="MIT",
-    keywords="asynchronous reinforcement learning policy gradient ppo appo impala ai",
-    project_urls={
-        "Github": "https://github.com/alex-petrenko/sample-factory",
-        "Videos": "https://sites.google.com/view/sample-factory",
-    },
+    ,
     install_requires=[
         "numpy>=1.18.1,<2.0",
         "torch>=1.9,<3.0,!=1.13.0",
@@ -50,12 +41,10 @@ setup(
         "psutil>=5.7.0",
         "threadpoolctl>=2.0.0",
         "colorlog",
-        # "faster-fifo>=1.4.2,<2.0",  <-- installed by signal-slot-mp
         "signal-slot-mp>=1.0.3,<2.0",
         "filelock",
         "opencv-python",
-        "wandb>=0.12.9",
-        "huggingface-hub>=0.10.0,<1.0",
+        
     ],
     extras_require={
         # some tests require Atari and Mujoco so let's make sure dev environment has that
@@ -67,7 +56,6 @@ setup(
         "envpool": _envpool_deps,
         "mujoco": _mujoco_deps,
         "vizdoom": ["vizdoom<2.0", "gymnasium[classic_control]"],
-        # "dmlab": ["dm_env"],  <-- these are just auxiliary packages, the main package has to be built from sources
     },
     package_dir={"": "./"},
     packages=setuptools.find_packages(where="./", include=["sample_factory*", "sf_examples*"]),
